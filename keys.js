259084711e445e01b6259592e76a4b3fe8dc9764
Sakina Shivji <shivjisakina@gmail.com>
2017-06-14 15:43:40 -0400

    added access keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<DsCvSwV5XGrwpfR3uMuAburHp>',
+  consumer_secret: '<TtYe84AecaZbwUgJxxYxRzZUR7NUHsYJ30VYdMCLGl2MZHBQAx>',
+  access_token_key: '<875074148858507270-DkoNqSStBbmtyYQOb5whMabvYqJ2Gli>',
+  access_token_secret: '<Dn7o60NtXNBwoTmvDCU4IA4LAzig1BxhvtM4RpjXhorJV>',
 }
