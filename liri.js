bad3255a384596ff5580e97ae35a878eec1d63e2
Sakina Shivji <shivjisakina@gmail.com>
2017-06-14 22:01:09 -0400

    added tweet times for loop

--- liri.js
+++ liri.js
@@ -72,6 +72,10 @@ if (process2 === "my-tweets") {
             console.log(tweets[i].text);
         }
 
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at);
+        }
+
     });
 }
 
