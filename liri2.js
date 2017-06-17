262031c87fe8d2ce46ef330ba33f1437bbbf2026
Sakina Shivji <shivjisakina@gmail.com>
2017-06-16 23:45:56 -0400

    added image of my-tweets for readme

--- liri2.js
+++ liri2.js
@@ -147,14 +147,16 @@ function omdb() {
 
 // FS
 
-function fs() {
+function readfile() {
 
     // We will read the existing bank file
-    fs.readFile("bank.txt", "utf8", function(err, data) {
+    fs.readFile("random.txt", "utf8", function(err, data) {
         if (err) {
             return console.log(err);
         }
 
-}
+        console.log(data)
+    });
+};
 
 //----------------------------------------------------------------------------------------------------------------------
